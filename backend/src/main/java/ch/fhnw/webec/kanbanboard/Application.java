ef5907943307597aff74a7445fe3e8361df272ea
Adrian Wilhelm <adrian.wilhelm@students.fhnw.ch>
2021-11-08 23:10:34 +0100

    Docker backend

--- backend/src/main/java/ch/fhnw/webec/kanbanboard/Application.java
+++ backend/src/main/java/ch/fhnw/webec/kanbanboard/Application.java
@@ -2,9 +2,17 @@ package ch.fhnw.webec.kanbanboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Application {
+
+    @GetMapping("/")
+    public String home() {
+        return "Kanban Board Backend API";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
