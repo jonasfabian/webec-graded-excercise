26158d901dbce480e9a2196965f81bcbba869036
Adrian Wilhelm <adrian.wilhelm@students.fhnw.ch>
2021-11-08 13:11:22 +0100

    Backend Setup

--- backend/src/main/java/ch/fhnw/webec/kanbanboard/Application.java
+++ backend/src/main/java/ch/fhnw/webec/kanbanboard/Application.java
@@ -1,11 +1,13 @@
-package ch.fhnw.webec.exercise;
+package ch.fhnw.webec.kanbanboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
 }
--- backend/src/test/java/ch/fhnw/webec/kanbanboard/ApplicationTests.java
+++ backend/src/test/java/ch/fhnw/webec/kanbanboard/ApplicationTests.java
@@ -1,10 +1,13 @@
-package ch.fhnw.webec.exercise;
+package ch.fhnw.webec.kanbanboard;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ApplicationTests {
-	@Test
-	void contextLoads() {}
+
+    @Test
+    void contextLoads() {
+    }
+
 }
